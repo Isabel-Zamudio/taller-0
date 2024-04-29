d375554bd37f532d20ee5b2fe2ae125f084ab6d1
Natalia Isabel Zamudio Muñoz <73199137+Isabel-Zamudio@users.noreply.github.com>
2024-04-29 18:19:38 -0500

    Update t04.js

--- JS/t04.js
+++ JS/t04.js
@@ -1,10 +1,2 @@
 //Escribe un programa que calcule el factorial de un número. 
 
-var f = [];
-function factorial (n) {
-  if (n == 0 || n == 1)
-    return 1;
-  if (f[n] > 0)
-    return f[n];
-  return f[n] = factorial(n-1) * n;
-}
