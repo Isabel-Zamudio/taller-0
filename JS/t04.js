0fbf306327628e5c9149c8f581acb1d9ebce3cde
Natalia Isabel Zamudio Muñoz <73199137+Isabel-Zamudio@users.noreply.github.com>
2024-06-04 15:57:07 -0500

    Update t04.js

--- JS/t04.js
+++ JS/t04.js
@@ -1,2 +1,11 @@
 //Escribe un programa que calcule el factorial de un número. 
 
+let numero = parseInt(prompt("Ingresa un numero: ))
+    if (numero == 0 || numero == 1) {
+        return 1;
+    } else {
+      return numero * factorial(numero - 1);
+    }
+}
+
+console.log("El factorial de " + numero + " es: " + factorial(numero));
